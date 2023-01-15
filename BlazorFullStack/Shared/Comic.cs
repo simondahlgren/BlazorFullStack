043e74e56208224de8a96246de176591b71f8284
S <S@DESKTOP-973UE4F>
2023-01-15 15:33:22 +0100

    Add project files.

---
+++ BlazorFullStack/Shared/Comic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorFullStack.Shared
+{
+    public class Comic
+    {
+        public int id { get; set; }
+        public string name { get; set; } = string.Empty;
+    }
+}
